811a3c06d8dc44280ad53c0d295b81e57bfb0db3
feizzulla <feizzulla@yandex.ru>
2024-07-18 20:42:09 +0300

    generateEnemies & enemyCreateRandomizer Functions

--- modules/generateEnemies.js
+++ modules/generateEnemies.js
@@ -1,1 +1,15 @@
-console.log("generateEnemies Init");
+//! Генерация Врагов
+function generateEnemies(timeOut) {
+  const timerId = setInterval(() => {
+    new Enemy();
+  }, timeOut);
+
+  //   setTimeout(() => {
+  //     clearInterval(timerId);
+  //   }, 5000);
+}
+
+//! Рандомизация интервала.
+function enemyCreateRandomizer(min, max) {
+  return Math.round(min + Math.random() * max);
+}
