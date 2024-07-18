9c17d332966dc08a948fb816be650724e75fff94
feizzulla <feizzulla@yandex.ru>
2024-07-18 20:51:31 +0300

    run setup

--- run.js
+++ run.js
@@ -1,2 +1,25 @@
 //! Файл запуска игры
-console.log("run init");
+const keypress = require("keypress");
+
+const playGame = () => {
+  const field = createField(fieldSize);
+
+  keypress(process.stdin);
+
+  process.stdin.on("keypress", function (ch, key) {
+    movePlayer(key.name);
+    displayField(field, playerPosition);
+
+    if (key && key.ctrl && key.name == "c") {
+      console.log("ПОТРАЧЕНО!");
+      process.stdin.pause();
+    }
+  });
+  2;
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+
+  displayField(field, playerPosition);
+};
+
+playGame();
